9efa041f6010993dcc0de1bf2e6859fee03a98bd
96kpan <katie@Katies-MacBook-Pro-4.local>
2016-11-15 06:36:51 -0700

    initial commit

---
+++ controller.php
@@ -0,0 +1,35 @@
+<?php
+// This file contains a bridge between the view and the model and redirects back to the proper page
+// with after processing whatever form this codew absorbs. This is the C in MVC, the Controller.
+//
+// Authors: Rick Mercer and Hassanain Jamal
+// 
+// TODO: Add control the new expected behavior to
+// register
+// log in
+// flag one quote
+// unflag all quotes
+// log out
+//
+require_once './DataBaseAdaptor.php';
+
+if (isset ( $_POST ['author'] ) && isset ( $_POST ['quote'] )) {
+	$author = $_POST ['author'];
+	$quote = $_POST ['quote'];
+	$myDatabaseFunctions->addNewQuote ( $quote, $author );
+	header ( "Location: ./index.php?mode=showQuotes" );
+} elseif (isset ( $_POST ['action'] ) && isset ( $_POST ['ID'] )) {
+	$action = $_POST ['action'];
+	$ID = $_POST ['ID'];
+	if ($action === 'increase') {
+		$myDatabaseFunctions->raiseRating ( $ID );
+	}
+	if ($action === 'decrease') {
+		$myDatabaseFunctions->lowerRating ( $ID );
+	}
+	if ($action === 'flag') {
+		$myDatabaseFunctions->flag ( $ID );
+	}
+	  header ( "Location: ./index.php?mode=showQuotes" );
+}
+?>
---
+++ showQuotes.php
@@ -0,0 +1,60 @@
+<!-- 
+This is the home page for Final Project, Part 1, Fall 2016 Quotes Enhanced.  
+
+Any user can flag quotes, add quotes, or register as a user.  If registered and logged in, the
+user can unflag all quotes to see all quotes in the data base. Yes some of the behavior is 
+contrived, in order to review many concepts in the context of a larger web service.  
+
+File name quotes.php 
+
+Show the home page view with tabs to login, registration, and to add a new quote and most importantly to
+show all unflagged quotes in the data base sorted by ranking
+    
+Author: Rick Mercer
+-->
+
+<?php
+require_once './DataBaseAdaptor.php';
+$arrayOfQuotes = $myDatabaseFunctions->getQuotesAsArray ();
+?>
+
+<h1>Quotes</h1>
+
+<!-- Add a horizontal menu -->
+
+<a href="addQuote.html">Add Quote</a>
+	<br>
+
+<?php
+session_start (); // Need this in each file before $_SESSION['key'] is used.
+?>
+
+<!--  Show all quotes on a separate row -->
+<?php foreach($arrayOfQuotes as $quote) { ?>
+
+<div class="container">
+		 <?= '"'. $quote['quote'] . '"' ?>  
+     <br>
+	<p class="author"> 
+     &nbsp;&nbsp;--
+     <?= $quote['author'] ?>  
+     <br>
+	</p>
+
+	<form action="controller.php" method="post">
+		<input type="hidden" name="ID" value="<?= $quote['id']?>">
+		&nbsp;&nbsp;&nbsp;
+		<button name="action" value="increase">+</button>
+
+		 <span id="rating"> <?= $quote['rating']?> </span>
+		<button name="action" value="decrease">-</button>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	</form>
+
+</div>
+<br>
+
+<?php } // End for loop  ?>
+
+</body>
+</html>
